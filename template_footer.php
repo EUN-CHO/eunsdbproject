282dc37bebb552dafffb62ec79d91a8eb330ad08
EUN-CHO <lvmclr2750@naver.com>
2017-06-27 09:21:52 +0900

    Add files via upload

--- template_footer.php
+++ template_footer.php
@@ -1,17 +1,1 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Store Home Page</title>
-<link rel="stylesheet" href="style/style.css" type="text/css" media="screen" />
-</head>
-
-<body>
-<div align="center" id="mainWrapper">
-  <?php include_once("template_header.php");?>
-  <div id="pageContent">Test</div>
-  <?php include_once("template_footer.php");?>
-  
-</div>
-</body>
-</html>
+<div id="pageFooter">Test</div>
--- template_header.php
+++ template_header.php
@@ -1,16 +1,12 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Store Home Page</title>
-<link rel="stylesheet" href="style/style.css" type="text/css" media="screen" />
-</head>
-
-<body>
-<div align="center" id="mainWrapper">
-  
-  <div id="pageContent">Test</div>
-  <div id="pageFooter">Test</div>
+<div id="pageHeader"><table width="662" border="0">
+  <tbody>
+    <tr>
+      <td width="199" height="46"><a href="index.php"><img src="style/logo.jpg" width="199" height="51" alt=""/></a></td>
+      <td width="453">&nbsp;</td>
+    </tr>
+    <tr>
+      <td height="28" colspan="2"><a href="index.php">Home</a> <a href="#">Link 1</a> <a href="#">Link 2</a> <a href="#">Link3</a></td>
+      </tr>
+  </tbody>
+</table>
 </div>
-</body>
-</html>
